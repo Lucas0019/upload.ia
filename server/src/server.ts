d802b44ac48c8077075edda628755dc37a62c35d
lucas <lxavier.00@outlook.com>
2023-09-13 22:49:59 -0300

    feat: finish api methods backend service

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,24 @@
+import {fastifyCors } from '@fastify/cors'
+
 import { fastify } from 'fastify'
+
+import { getAllPromptsRoute } from './routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload-video'
+import { createTranscriptionRoute } from './routes/create-transcription'
+import { generateAiCompletionRoute } from './routes/generate-ai-completion'
 
 const app = fastify()
 
-app.get('/', async () => {
-  return { hello: 'IA' }
+app.register(fastifyCors, {
+  origin: '*',
+  // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  // allowedHeaders: ['Content-Type', 'Authorization'],
 })
+
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAiCompletionRoute)
 
 app.listen({
   port: 3333,
